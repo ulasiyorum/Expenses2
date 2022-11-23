ebd679e96e6fa40480419398b0d46c9d0d15c9d3
Ulaş Gergerli <20200808049@ogr.akdeniz.edu.tr>
2022-11-23 22:53:47 +0300

    effects on android & iOS

--- Expenses2.Android/Effects/SelectedEffect.cs
+++ Expenses2.Android/Effects/SelectedEffect.cs
@@ -11,6 +11,9 @@ using System.Text;
 using Xamarin.Forms.Platform.Android;
 using Xamarin.Forms;
 using Expenses2.Droid.Effects;
+using Android.Graphics.Drawables;
+using Android.Graphics;
+using System.ComponentModel;
 
 [assembly: ResolutionGroupName("VerdantGames")]
 [assembly: ExportEffect(typeof(SelectedEffect),"SelectedEffect")]
@@ -18,9 +21,34 @@ namespace Expenses2.Droid.Effects
 {
     public class SelectedEffect : PlatformEffect
     {
+        Android.Graphics.Color selectedColor;
         protected override void OnAttached()
         {
-            
+            selectedColor = new Android.Graphics.Color(229, 235, 234);
+        }
+
+        protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+        {
+            base.OnElementPropertyChanged(args);
+
+            try
+            {
+                if (args.PropertyName == "IsFocused")
+                {
+                    if (((ColorDrawable)Control.Background).Color != selectedColor)
+                    {
+                        Control.SetBackgroundColor(selectedColor);
+                    }
+                    else
+                    {
+                        Control.SetBackgroundColor(Android.Graphics.Color.White);
+                    }
+                }
+            }
+            catch (InvalidCastException)
+            {
+                Control.SetBackgroundColor(selectedColor);
+            }
         }
 
         protected override void OnDetached()
--- Expenses2.iOS/Effect/SelectedEffect.cs
+++ Expenses2.iOS/Effect/SelectedEffect.cs
@@ -2,6 +2,7 @@
 using Foundation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using UIKit;
@@ -13,13 +14,29 @@ using Xamarin.Forms.Platform.iOS;
 [assembly: ExportEffect(typeof(SelectedEffect),"SelectedEffect")]
 namespace Expenses2.iOS.Effect
 {
-    internal class SelectedEffect : PlatformEffect
+    public class SelectedEffect : PlatformEffect
     {
+        UIColor selectedColor;
         protected override void OnAttached()
         {
-           
+            selectedColor = new UIColor(176 / 255, 152 / 255, 164 / 255, 1);
         }
+        protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+        {
+            base.OnElementPropertyChanged(args);
 
+            if(args.PropertyName == "IsFocused")
+            {
+                if(Control.BackgroundColor != selectedColor)
+                {
+                    Control.BackgroundColor = selectedColor;
+                }
+                else
+                {
+                    Control.BackgroundColor = UIColor.White;
+                }
+            }
+        }
         protected override void OnDetached()
         {
             
